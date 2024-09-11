8f1547de114856237e645464b4e9dfab56a9f9b7
Eric Li <eric.li.8736@gmail.com>
2024-09-11 17:11:19 -0500

    Add yazi script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,15 @@ function ll
     lsd -al --group-dirs first
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 
 
 # Keep this at the end
