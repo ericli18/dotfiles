a872eb3f07ad550cd49df18c0607b5c6f259b9fe
Eric Li <eric.li.8736@gmail.com>
2025-01-05 13:41:50 -0600

    Remove nvm in favor of mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,14 +54,15 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/ericli/miniconda3/bin/conda
-    eval /home/ericli/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/ericli/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/ericli/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/ericli/miniconda3/bin" $PATH
-    end
-end
+# if status --is-interactive
+#     if test -f /home/ericli/miniconda3/bin/conda
+#         eval /home/ericli/miniconda3/bin/conda "shell.fish" hook $argv | source
+#     else
+#         if test -f "/home/ericli/miniconda3/etc/fish/conf.d/conda.fish"
+#             . "/home/ericli/miniconda3/etc/fish/conf.d/conda.fish"
+#         else
+#             set -x PATH /home/ericli/miniconda3/bin $PATH
+#         end
+#     end
+# end
 # <<< conda initialize <<<
-
