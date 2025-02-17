e6f2bcbd30305b7f2b317c9cd49f7b386a0ef215
Eric Li <eric.li.8736@gmail.com>
2025-02-17 00:40:04 -0600

    Snippets and custom mode module

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,29 +1,111 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "gruvchad",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    theme = "gruvchad",
 }
 
- M.ui = {
-   statusline = {
-     theme = "vscode_colored",
-   },
- }
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.ui = {
+    statusline = {
+        theme = "vscode_colored",
+
+        -- order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "lsp" },
+        -- order = { "mode", "kirby_face" },
+        order = { "custom_mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+        modules = {
+
+            custom_mode = function()
+                local mode = vim.fn.mode()
+
+                local mode_names = {
+                    n      = "NORMAL",
+                    i      = "INSERT",
+                    v      = "VISUAL",
+                    ['']  = "VISUAL-BLOCK",
+                    V      = "V-LINE",
+                    c      = "COMMAND",
+                    no     = "NORMAL (operator pending)",
+                    s      = "SELECT",
+                    S      = "S-LINE",
+                    ['']  = "S-BLOCK",
+                    ic     = "INSERT (completion)",
+                    R      = "REPLACE",
+                    Rv     = "V-REPLACE",
+                    cv     = "COMMAND",
+                    ce     = "COMMAND",
+                    r      = "PROMPT",
+                    rm     = "MORE",
+                    ['r?'] = "CONFIRM",
+                    ['!']  = "SHELL",
+                    t      = "TERMINAL",
+                }
+
+                -- The Kirby face mapping.
+                local mode_kirby = {
+                    n      = "<(•ᴗ•)>",
+                    i      = "<(•o•)>",
+                    v      = "(v•-•)v",
+                    [""]  = "(v•-•)>",
+                    V      = "(>•-•)>",
+                    c      = "(>*-*)>",
+                    no     = "<(•ᴗ•)>",
+                    s      = "(>*-*)>",
+                    S      = "(>*-*)>",
+                    [""]  = "(>*-*)>",
+                    ic     = "(>*-*)>",
+                    R      = "(>*-*)>",
+                    Rv     = "(>*-*)>",
+                    cv     = "<(•ᴗ•)>",
+                    ce     = "<(•ᴗ•)>",
+                    r      = "(>*-*)>",
+                    rm     = "(>*-*)>",
+                    ["r?"] = "(>*-*)>",
+                    ["!"]  = "<(•ᴗ•)>",
+                    t      = "<(•ᴗ•)>",
+                }
+
+                local mode_hl = {
+                    n      = "St_NormalMode",
+                    i      = "St_InsertMode",
+                    v      = "St_VisualMode",
+                    [""]  = "St_VisualMode",
+                    V      = "St_VisualMode",
+                    c      = "St_CommandMode",
+                    no     = "St_NormalMode",
+                    s      = "St_SelectMode",
+                    S      = "St_SelectMode",
+                    ['']  = "St_SelectMode",
+                    ic     = "St_InsertMode",
+                    R      = "St_ReplaceMode",
+                    Rv     = "St_ReplaceMode",
+                    cv     = "St_CommandMode",
+                    ce     = "St_CommandMode",
+                    r      = "St_PromptMode",
+                    rm     = "St_PromptMode",
+                    ['r?'] = "St_PromptMode",
+                    ['!']  = "St_ShellMode",
+                    t      = "St_TerminalMode",
+                }
+
+                local mode_name = mode_names[mode] or mode
+                local kirby = mode_kirby[mode] or mode
+                local hl = mode_hl[mode] or "St_NormalMode"
+
+                return string.format("%%#%s# %s %s ", hl, mode_name, kirby)
+            end,
+
+        },
+    },
+
+    tabufline = {
+        enabled = true,
+        order = { "treeOffset", "buffers", "tabs" },
+        lazyload = false,
+    },
+
+    cheatsheet = {
+        theme = "simple", -- simple/grid
+    },
+}
 
 return M
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,146 +1,166 @@
 return {
-  -- lazy.nvim
-  {
-    'nvim-telescope/telescope.nvim',
-    enabled = false
-  },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      picker = {
-        win = {
-          input = {
-            keys = {
-              ["<Esc>"] = { "close", mode = { "n", "i" } },
-            }
-          }
-        }
-      },
-      bigfile = { enabled = true },
-      quickfile = { enabled = true },
-    },
-    keys = {
-      -- Top Pickers & Explorer
-      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-      { "<C-p>",           function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-      -- { "<leader>,",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
-      { "<leader>fg",      function() Snacks.picker.grep() end,  desc = "Grep" },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
+    -- lazy.nvim
+    --
+    {
+        "L3MON4D3/LuaSnip",
+        config = function()
+            require "nvchad.configs.luasnip"
+
+            local ls = require "luasnip"
+
+            ls.filetype_extend("javascriptreact", { "html" })
+            ls.filetype_extend("typescriptreact", { "html" })
+
+            require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets/vscode" } }
         end,
-      })
-    end,
-  },
-  {
-    "hiphish/rainbow-delimiters.nvim",
-    version = "*", -- Pin to GitHub releases
-    lazy = false,
-    -- enabled = false,
-    config = function()
-      require('rainbow-delimiters.setup').setup {
-        highlight = {
-          'RainbowDelimiterCyan',
-          'RainbowDelimiterYellow',
-          'RainbowDelimiterBlue',
-          'RainbowDelimiterOrange',
-          'RainbowDelimiterGreen',
-          'RainbowDelimiterViolet',
-          'RainbowDelimiterRed',
-        },
-      }
-    end,
-  },
-  {
-    "amitds1997/remote-nvim.nvim",
-    version = "*",                     -- Pin to GitHub releases
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- For standard functions
-      "MunifTanjim/nui.nvim",          -- To build the plugin UI
-      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
     },
-    config = true,
-  },
-  {
-    'xeluxee/competitest.nvim',
-    dependencies = 'MunifTanjim/nui.nvim',
-    config = function()
-      require('competitest').setup {
-        -- testcases_directory = "./.tests",
-        testcases_use_single_file = true,
-      }
-    end,
-    lazy = false
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.treesitter")
-    end,
-  },
+    {
+        'nvim-telescope/telescope.nvim',
+        enabled = false
+    },
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            picker = {
+                win = {
+                    input = {
+                        keys = {
+                            ["<Esc>"] = { "close", mode = { "n", "i" } },
+                        }
+                    }
+                }
+            },
+            bigfile = { enabled = true },
+            quickfile = { enabled = true },
+        },
+        keys = {
+            -- Top Pickers & Explorer
+            { "<leader><space>", function() Snacks.picker.files() end,   desc = "Smart Find Files" },
+            { "<C-p>",           function() Snacks.picker.files() end,   desc = "Smart Find Files" },
+            { "<leader>,",       function() Snacks.picker.buffers() end, desc = "Buffers" },
+            { "<leader>fg",      function() Snacks.picker.grep() end,    desc = "Grep" },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "VeryLazy",
+                callback = function()
+                    -- Setup some globals for debugging (lazy-loaded)
+                    _G.dd = function(...)
+                        Snacks.debug.inspect(...)
+                    end
+                    _G.bt = function()
+                        Snacks.debug.backtrace()
+                    end
+                    vim.print = _G.dd -- Override print to use snacks for `:=` command
+                end,
+            })
+        end,
+    },
+    {
+        "hiphish/rainbow-delimiters.nvim",
+        version = "*", -- Pin to GitHub releases
+        lazy = false,
+        -- enabled = false,
+        config = function()
+            require('rainbow-delimiters.setup').setup {
+                highlight = {
+                    'RainbowDelimiterCyan',
+                    'RainbowDelimiterYellow',
+                    'RainbowDelimiterBlue',
+                    'RainbowDelimiterOrange',
+                    'RainbowDelimiterGreen',
+                    'RainbowDelimiterViolet',
+                    'RainbowDelimiterRed',
+                },
+            }
+        end,
+    },
+    {
+        "amitds1997/remote-nvim.nvim",
+        version = "*",                       -- Pin to GitHub releases
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- For standard functions
+            "MunifTanjim/nui.nvim",          -- To build the plugin UI
+            "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+        },
+        config = true,
+    },
+    {
+        'xeluxee/competitest.nvim',
+        dependencies = 'MunifTanjim/nui.nvim',
+        config = function()
+            require('competitest').setup {
+                -- testcases_directory = "./.tests",
+                testcases_use_single_file = true,
+                compile_command = {
+                    c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+                    cpp = { exec = "g++", args = { "-DLOCAL", "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+                    rust = { exec = "rustc", args = { "$(FNAME)" } },
+                    java = { exec = "javac", args = { "$(FNAME)" } },
+                },
+            }
+        end,
+        lazy = false
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
 
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require("configs.lspconfig")
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-lspconfig" },
-    config = function()
-      require("configs.mason-lspconfig")
-    end,
-  },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
 
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.lint")
-    end,
-  },
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
 
-  {
-    "rshkarin/mason-nvim-lint",
-    event = "VeryLazy",
-    dependencies = { "nvim-lint" },
-    config = function()
-      require("configs.mason-lint")
-    end,
-  },
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
 
-  {
-    "stevearc/conform.nvim",
-    -- event = "BufWritePre",
-    config = function()
-      require("configs.conform")
-    end,
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = "BufWritePre",
+        config = function()
+            require("configs.conform")
+        end,
+    },
 
-  {
-    "zapling/mason-conform.nvim",
-    event = "VeryLazy",
-    dependencies = { "conform.nvim" },
-    config = function()
-      require("configs.mason-conform")
-    end,
-  },
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
 }
