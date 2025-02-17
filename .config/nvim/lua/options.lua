0d5c626dcf0fd23bdccfb940c2e2ecba58c6e243
Eric Li <eric.li.8736@gmail.com>
2025-02-17 00:39:31 -0600

    Add competitive programming snippets

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,19 +1,23 @@
 require "nvchad.options"
 
 -- add yours here!
+--
+vim.g.vscode_snippets_path = vim.fn.stdpath("config") .. "/snippets/vscode"
 
 local opt = vim.o
-opt.linebreak = true -- Wrap lines at convenient points
-opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = "a" -- Enable mouse mode
+opt.linebreak = true      -- Wrap lines at convenient points
+opt.list = true           -- Show some invisible characters (tabs...
+opt.mouse = "a"           -- Enable mouse mode
 
 opt.relativenumber = true -- Relative line numbers
 
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
+opt.smartcase = true      -- Don't ignore case with capitals
+opt.smartindent = true    -- Insert indents automatically
 
-opt.termguicolors = true -- True color support
+opt.termguicolors = true  -- True color support
 
+opt.shiftwidth = 0
+opt.tabstop = 4
 
 local function augroup(name)
   return vim.api.nvim_create_augroup("nvim_" .. name, { clear = true })
