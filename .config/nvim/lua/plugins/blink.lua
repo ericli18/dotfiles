2affade41256c8fcba3c7d63450f6315b0f90cef
Eric Li <eric.li.8736@gmail.com>
2025-10-28 16:35:23 -0500

    Don't check when adding jsregexp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,12 +6,7 @@ return {
     {
       'L3MON4D3/LuaSnip',
       version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
+      build = 'make install_jsregexp',
       dependencies = {},
 
       config = function()
